09df3ce3169d1425b5eaa96b75e7b541c48c5892
Fenris <thefenrislycaon@gmail.com>
2022-01-27 17:54:19 +0530

    'refactor'

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def gen_frames():
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode(".jpg", frame)-
+            ret, buffer = cv2.imencode(".jpg", frame)
             frame = buffer.tobytes()
             yield (b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
 
