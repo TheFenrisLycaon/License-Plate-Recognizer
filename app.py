58fe87ec003583615d1294c987b040678642e658
Fenris <thefenrislycaon@gmail.com>
2022-01-31 20:15:03 +0530

    Stash

--- app.py
+++ app.py
@@ -3,16 +3,14 @@ from flask import Flask, Response, render_template
 
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(
-    "http://182.65.247.87:8080/stream169"
-)
+camera = cv2.VideoCapture("http://182.65.247.87:8080/stream169")
 
 
 def gen_frames():
     while True:
         success, frame = camera.read()
         if not success:
-            break
+            camera = cv2.VideoCapture("http://182.65.247.87:8080/stream169")
         else:
             ret, buffer = cv2.imencode(".jpg", frame)
             frame = buffer.tobytes()
@@ -23,7 +21,7 @@ def gen_frames():
 def index():
     return render_template("index.html")
 
-
+,
 @app.route("/video_feed")
 def video_feed():
     return Response(gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
